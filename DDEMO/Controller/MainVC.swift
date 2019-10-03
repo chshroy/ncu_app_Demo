a239f62e275b0e61f74511258ea65cbe17fec38c
chshroy <bikingyes@gmail.com>
2019-10-03 20:39:51 +0800

    10.03 20:39

--- DDEMO/Controller/MainVC.swift
+++ DDEMO/Controller/MainVC.swift
@@ -3,6 +3,7 @@ import Foundation
 
 
 class MainVC: UITableViewController {
+    // hello
     var items = [Item]()
     var title_text: String? = "User Name"
     @IBOutlet var userNameTitle: UINavigationItem!
